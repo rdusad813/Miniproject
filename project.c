f817b4f4dc7753dd7ae46d56c5cddfc97be3b9e1
rdusad813 <rdusad813@gmail.com>
2015-11-15 21:48:03 +0530

    Update project.c

--- project.c
+++ project.c
@@ -1,3 +1,24 @@
+
+/*****************************************************************************
+ * Copyright (C) Rishabh Dusad rdusad813@gmail.com
+ 
+ * This file is part of Grocery Store Management System.
+
+ *   Grocery Store Management System is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+
+ *   Grocery Store Management System is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+
+ *   You should have received a copy of the GNU General Public License
+ *   along with Grocery Store Management System.  If not, see <http://www.gnu.org/licenses/>.
+
+ *****************************************************************************/
+
 #include<fcntl.h>
 #include<errno.h>
 #include<stdlib.h>
